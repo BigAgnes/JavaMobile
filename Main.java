b73ee271b4e59a484f98fc1778f67c8316366ce8
BigAgnes <k.tc.7@yandex.ru>
2020-11-02 16:22:05 +0300

    Test #2
    Homework #2

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ import org.junit.Test;
 public class Main {
     public static void main(String[] args) {
         MainClassTest test = new MainClassTest();
-        test.testGetLocalNumber();
+        //test.testGetLocalNumber();
+        test.testGetClassNumber();
 
 
     }
--- MainClassTest.java
+++ MainClassTest.java
@@ -7,4 +7,10 @@ public class MainClassTest extends MainClass{
     {
         Assert.assertTrue( "This value is not 14!",getLocalNumber()==14);
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue( "Value less than 45",getClassNumber()>45);
+    }
 }
